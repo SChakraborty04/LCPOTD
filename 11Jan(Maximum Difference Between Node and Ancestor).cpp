62a559dd41a2014cd196c30549d5c8c5b5dce542
SChakraborty04 <120569942+SChakraborty04@users.noreply.github.com>
2024-01-11 14:23:02 +0530

    Create 11Jan(Maximum Difference Between Node and Ancestor).cpp

---
+++ 11Jan(Maximum Difference Between Node and Ancestor).cpp
@@ -0,0 +1,22 @@
+//Problem: https://leetcode.com/problems/maximum-difference-between-node-and-ancestor/description/?envType=daily-question&envId=2024-01-11
+
+int maxAncestorDiff(TreeNode* root) {
+        int ans=0;
+        const int inf =1e6;
+        function<pair<int,int>(TreeNode *)>dfs=[&](TreeNode*node)->pair<int,int>{
+            if(!node)
+            return {inf,-inf};
+            pair<int,int> left =dfs(node->left);
+            pair<int,int> right =dfs(node->right);
+
+            int MIN=min(left.first,right.first);
+            int MAX=max(left.second,right.second);
+
+            if(node->left or node->right)
+            ans=max({ans,abs(MIN-node->val),abs(MAX-node->val)});
+
+            return {min(MIN,node->val),max(MAX,node->val)};
+        };
+        dfs(root);
+        return ans;
+    }
